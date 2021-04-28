1bc5c7f30717ec98152046dde29cee704216d96c
Ganesh Vernekar <ganeshvern@gmail.com>
2021-04-28 20:48:27 +0530

    NGAlert: Send list of available ngalert notification channels via API
    Signed-off-by: Ganesh Vernekar <ganeshvern@gmail.com>

--- pkg/api/http_server.go
+++ pkg/api/http_server.go
@@ -38,6 +38,7 @@ import (
 	"github.com/grafana/grafana/pkg/services/live"
 	"github.com/grafana/grafana/pkg/services/live/pushhttp"
 	"github.com/grafana/grafana/pkg/services/login"
+	"github.com/grafana/grafana/pkg/services/ngalert/notifier"
 	"github.com/grafana/grafana/pkg/services/provisioning"
 	"github.com/grafana/grafana/pkg/services/quota"
 	"github.com/grafana/grafana/pkg/services/rendering"
@@ -98,6 +99,7 @@ type HTTPServer struct {
 	DataService            *tsdb.Service                           `inject:""`
 	PluginDashboardService *plugindashboards.Service               `inject:""`
 	AlertEngine            *alerting.AlertEngine                   `inject:""`
+	Alertmanager           *notifier.Alertmanager                  `inject:""`
 	Listener               net.Listener
 }
 
